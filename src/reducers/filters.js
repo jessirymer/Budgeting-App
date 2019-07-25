8cf63c01179e44a57f2e94d546b224e231d38ec6
Jessi Rymer <jessi.rymer@gmail.com>
2019-07-24 22:19:49 -0400

    Use moment to change default start and end date in filter reducer to start of month and end of month.

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,9 +1,11 @@
+import moment from "moment"
+
 // Filters Reducer
 const filtersReducerDefaultState = {
     text: "",
     sortBy: "amount",
-    startDate: undefined,
-    endDate: undefined
+    startDate: moment().startOf("month"),
+    endDate: moment().endOf("month")
 }
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch(action.type) {
